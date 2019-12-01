b027861742bc29104ca05d54eda70c6156680920
Ben Edwards <ben@simplicat.com>
2019-12-01 12:42:18 +0000

    Add mysql, docker compose
    Db data dir is mounted in project dir, so gitignore that

--- rufford_org/settings/dev.py
+++ rufford_org/settings/dev.py
@@ -4,14 +4,20 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#g$76xn(vz5xn3*_djf#goi@_!(cuovinc0spin25uoqc_=yj#'
+SECRET_KEY = "#g$76xn(vz5xn3*_djf#goi@_!(cuovinc0spin25uoqc_=yj#"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-
+DATABASES["legacy"] = {
+    "ENGINE": "django.db.backends.mysql",
+    "NAME": "rufford_org",
+    "HOST": "localhost",
+    "USER": "root",
+    "PASSWORD": "aslkd0923ddll",
+}
 try:
     from .local import *
 except ImportError:
